86925b582c950475364bd22fb11b1d3ba29b001d
AselaDarshan <aseladarshan@gmail.com>
2017-04-20 18:44:51 +0530

    Add draw url

--- config.js
+++ config.js
@@ -97,4 +97,5 @@ var config = { // eslint-disable-line no-unused-vars
     // How long we're going to wait, before going back to P2P after
     // the 3rd participant has left the conference (to filter out page reload)
     //backToP2PDelay: 5
+    etherpad_base: 'https://whiteboard.siplo.lk',
 };
